13c3f36c0d86fa99243a7e332d6e152167c68ca0
DanBonser <lynx.madrin@gmail.com>
2024-07-07 09:19:34 -0500

    Add sidekick actor type

--- scripts/sidekick-manager.js
+++ scripts/sidekick-manager.js
@@ -1,5 +1,5 @@
 Hooks.on('init', () => {
-    // Define the new actor type
+    // Register the new actor type by extending the Actor class
     CONFIG.Actor.documentClass = SidekickActor;
 
     // Add "Sidekick" to the actor type dropdown
@@ -7,68 +7,21 @@ Hooks.on('init', () => {
         ...CONFIG.Actor.typeLabels,
         "sidekick": "Sidekick"
     };
-
-    // Register the new actor type
-    Actors.registerSheet("dnd5e", SidekickActorSheet, {
-        types: ["sidekick"],
-        makeDefault: false
-    });
 });
 
+// Define the new SidekickActor class
 class SidekickActor extends Actor {
-    // Override any necessary methods here
+    // You can override necessary methods here, for now, we'll keep it simple
 }
 
+// Register the new sheet for the "sidekick" type
+Actors.registerSheet("dnd5e", SidekickActorSheet, {
+    types: ["sidekick"],
+    makeDefault: false
+});
+
+// Define a simple SidekickActorSheet class
 class SidekickActorSheet extends ActorSheet5eCharacter {
-    getData() {
-        const data = super.getData();
-        const sidekickLevels = game.settings.get("sidekick-manager", "sidekickLevels");
-        data.sidekickLevels = sidekickLevels[data.actor._id] || 1;
-        return data;
-    }
-
-    activateListeners(html) {
-        super.activateListeners(html);
-        html.find(".sidekick-level").change(this._onSidekickLevelChange.bind(this));
-    }
-
-    async _onSidekickLevelChange(event) {
-        const newLevel = parseInt(event.target.value);
-        const actorId = this.actor._id;
-        const sidekickLevels = game.settings.get("sidekick-manager", "sidekickLevels");
-        sidekickLevels[actorId] = newLevel;
-        await game.settings.set("sidekick-manager", "sidekickLevels", sidekickLevels);
-
-        // Update the actor stats based on the new level
-        await this._updateActorStats(newLevel);
-    }
-
-    async _updateActorStats(level) {
-        const updates = {};  // Object to store updates
-        
-        // Define the changes based on the level and class
-        // Note: This example only covers level 1 for the Expert sidekick
-        if (level === 1) {
-            updates['data.abilities.str.value'] = 10;
-            updates['data.abilities.dex.value'] = 15;
-            updates['data.abilities.con.value'] = 12;
-            updates['data.abilities.int.value'] = 13;
-            updates['data.abilities.wis.value'] = 10;
-            updates['data.abilities.cha.value'] = 14;
-            updates['data.skills.acr.value'] = 4;
-            updates['data.skills.prf.value'] = 4;
-            updates['data.skills.per.value'] = 4;
-            updates['data.skills.slt.value'] = 4;
-            updates['data.skills.ste.value'] = 4;
-            updates['data.attributes.hp.value'] = 11;
-            updates['data.attributes.hp.max'] = 11;
-            updates['data.attributes.ac.value'] = 14;
-            // Add more updates as needed
-        }
-
-        await this.actor.update(updates);
-    }
-
     static get defaultOptions() {
         const options = super.defaultOptions;
         options.classes = [...options.classes, "sidekick-manager"];
